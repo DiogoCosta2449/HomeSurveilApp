632919f80f90314443cde196a7741cc88a93b94d
Diogo Costa <diogo2449@gmail.com>
2025-06-11 18:44:19 +0100

    Fix Loop Splash

--- app/src/main/java/com/example/homesurveilapp/MainActivity.kt
+++ app/src/main/java/com/example/homesurveilapp/MainActivity.kt
@@ -10,12 +10,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        // Inicia o SplashActivity após 5 segundos (não recomendado normalmente)
-        Handler(Looper.getMainLooper()).postDelayed({
-            startActivity(Intent(this, SplashActivity::class.java))
-            finish()
-        }, 5000)
     }
 }
 
