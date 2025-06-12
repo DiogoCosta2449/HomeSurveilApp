e1f0acf940a12268a7aec35e24be3a4fa74f1ae8
Diogo Costa <diogo2449@gmail.com>
2025-06-12 19:21:58 +0100

    Botoes na mainactivity. Antonia funcional. Falta fazer o resto.

--- app/src/main/java/com/example/homesurveilapp/MainActivity.kt
+++ app/src/main/java/com/example/homesurveilapp/MainActivity.kt
@@ -1,15 +1,25 @@
+
 package com.example.homesurveilapp
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val layoutAntonia = findViewById<android.widget.LinearLayout>(R.id.layoutAntonia)
+        layoutAntonia.setOnClickListener {
+            val intent = Intent(this, UserAntonia::class.java)
+            startActivity(intent)
+        }
+
+        val layoutMariana = findViewById<android.widget.LinearLayout>(R.id.layoutMariana)
+        layoutAntonia.setOnClickListener {
+            val intent = Intent(this, UserAntonia::class.java)
+            startActivity(intent)
+        }
     }
 }
-
---
+++ app/src/main/java/com/example/homesurveilapp/user_mariana.kt
@@ -0,0 +1,11 @@
+package com.example.homesurveilapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class UserMariana : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.user_mariana)
+    }
+}
